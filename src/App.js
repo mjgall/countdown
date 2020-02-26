7c7606e0d3108d481a0b96626a9cbb8033512d08
Mike Gallagher <mike.gallagh@gmail.com>
2020-02-26 13:33:37 -0600

    change text

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import React from 'react';
 import './App.css';
 
 import moment from 'moment';
+import axios from 'axios';
 
 export default class App extends React.Component {
-  state = { daysUntilMarch8th: 11, earlierDate: null };
+  state = { daysUntilMarch8th: 11, earlierDate: null, sunsetToday: null };
 
-  componentDidMount = () => {
+  componentDidMount = async () => {
     const march9th = moment('2020-03-09');
     const today = moment();
     const daysUntil = march9th.diff(today, 'days');
     const previousDate = moment()
       .subtract(daysUntil, 'days')
       .format('dddd, MMMM D');
+    const sunsetData = await axios.get(
+      'https://api.sunrise-sunset.org/json?lat=44.986656&lng=-93.258133&date=today'
+    );
 
-    this.setState({ daysUntilMarch8th: daysUntil, earlierDate: previousDate });
+    this.setState({
+      daysUntilMarch8th: daysUntil,
+      earlierDate: previousDate
+    });
   };
 
   render = () => {
